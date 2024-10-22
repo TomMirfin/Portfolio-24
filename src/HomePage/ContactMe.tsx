dd00d80b22749eacf9c17edc92a15256b7a7b443
t.a.mirfin@gmail.com <t.a.mirfin@gmail.com>
2024-10-22 14:15:05 +0100

    fixing bits

--- src/HomePage/ContactMe.tsx
+++ src/HomePage/ContactMe.tsx
@@ -8,11 +8,11 @@ const ContactMe = () => {
     message: "",
   });
 
-  const handleChange = (e) => {
+  const handleChange = (e: any) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: any) => {
     e.preventDefault();
     emailjs
       .send(
